a6f79b759805f23e4d3351293550816edb768c7e
Valentin Alekseev <valentin.alekseev@fleetcor.ru>
2022-06-26 18:28:50 +0300

    My tests

---
+++ src/test/java/ru/yandex/praktikum/CalculatorTest.java
@@ -0,0 +1,34 @@
+package ru.yandex.praktikum;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class) // указали, что тесты будет запускать раннер Parameterized
+
+public class CalculatorTest {
+    private final int firstNumber;
+    private final int secondNumber;
+    private final int expected;
+
+    public CalculatorTest(int firstNumber, int secondNumber, int expected) {
+        this.firstNumber = firstNumber;
+        this.secondNumber = secondNumber;
+        this.expected = expected;
+    }
+
+    @Parameterized.Parameters // добавили аннотацию
+    public static Object[][]  getSumData() {
+        return new Object[][] {
+                { 1, 9, 10},
+                { 1, 0, 1},
+        };
+    }
+    @Test
+    public void shouldBeSum() {
+        Calculator calculator = new Calculator();
+        int actual = calculator.sum(firstNumber, secondNumber);
+        assertEquals(expected, actual);
+    }
+}
